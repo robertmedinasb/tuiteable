80c8fb08604d3e1624bde8fb2b16370d39f70d1e
Albert Castellano Moreno <acastemoreno@gmail.com>
2020-06-03 15:16:36 -0500

    Revert "Db setup"

--- app/models/provider.rb
+++
@@ -1,3 +0,0 @@
-class Provider < ApplicationRecord
-  belongs_to :user
-end
--- app/models/tweet.rb
+++
@@ -1,7 +0,0 @@
-class Tweet < ApplicationRecord
-  ## Association
-  belongs_to :owner, class_name: 'User'
-  has_many :comments
-  has_many :commentators, through: :comments, source: :user
-  has_and_belongs_to_many :likers, join_table: "likes", class_name: "User",
-end
