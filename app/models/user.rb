ea9f448077d1535a7ec6ae6d91f935f2278d4756
Albert <acastemoreno@gmail.com>
2020-06-03 14:30:26 -0500

    device and omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable, :rememberable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable, :omniauthable, omniauth_providers: %i[facebook github]
+
+  def self.from_omniauth(auth)
+    user = where(email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.username = auth.info.name.downcase.gsub(/\s/,"") + rand(1..10000).to_s
+      user.password = Devise.friendly_token[0, 20]
+    end
+
+    Provider.where(name: auth.provider, uid: auth.uid, user: user).first_or_create
+
+    user
+  end
 end
--- db/migrate/20200603163638_create_users.rb
+++ db/migrate/20200603163638_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, null: false
       t.string :name
       t.string :email
       t.string :location
@@ -11,6 +11,5 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     end
 
     add_index :users, :username, unique: true
-    add_index :users, :email, unique: true
   end
 end
