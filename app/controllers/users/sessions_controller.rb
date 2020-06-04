44e3959d4e69449470f3810acdff470faa1814d2
robertmedinasb <robertmedib1@gmail.co>
2020-06-04 02:05:46 -0500

    Adding login inside the side bar is already finish

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate(auth_options)
+
+    if resource
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      flash[:alert] = 'User and password not matched.'
+      redirect_to root_path
+      # Authentication fails, redirect the user to the root page
+    end
+     # end
+
+     # DELETE /resource/sign_out
+     # def destroy
+     #   super
+   end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
