81e8c203f2b78bebf8fa2eada711c04afbc0bfbb
robertmedinasb <robertmedib1@gmail.co>
2020-06-05 02:34:15 -0500

    Fixed some issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,14 @@
 
 class UsersController < ApplicationController
   def show
-    if current_user
-      if params[:id].nil?
-        @user = User.find(current_user.id)
-        @tuits = @user.tuits
-        @likes = @user.likes.map { |like| Tuit.find(like[:tuit_id]) }
-      else
-        @user = User.find(params[:id])
-        @tuits = @user.tuits
-        @likes = @user.likes.map { |like| Tuit.find(like[:tuit_id]) }
-      end
+    if params[:id].nil?
+      @user = User.find(current_user.id)
+      @tuits = @user.tuits
+      @likes = @user.likes.map { |like| Tuit.find(like[:tuit_id]) }
+    else
+      @user = User.find(params[:id])
+      @tuits = @user.tuits
+      @likes = @user.likes.map { |like| Tuit.find(like[:tuit_id]) }
     end
   end
 
