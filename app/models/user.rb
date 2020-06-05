3e0fb6a105755abc101a5d966283a373b0cbaba1
robertmedinasb <robertmedib1@gmail.co>
2020-06-05 01:11:02 -0500

    Merge all changes from gerard and Fer and added log-out button and sticky position for nav and explorer title also login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,12 +20,11 @@ class User < ApplicationRecord
   has_one_attached :avatar
   def self.from_omniauth(auth)
     downloaded_image = open(auth.info.image)
-    p auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.location = 'Peru'
       user.email = auth.info.email
       user.bio = auth.info.user_about_me
-      user.name = auth.info.name
+      user.name = auth.info.namenav
       user.username = auth.info.name.downcase.gsub(/\s/, '') + rand(1..100).to_s
       user.password = Devise.friendly_token[0, 20]
       user.avatar.attach(io: downloaded_image, filename: 'avatar.jpg', content_type: downloaded_image.content_type)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/profile(/:id)', to: "users#show"
+  get 'users/profile(/:id)', to: 'users#show'
   get 'tuits/index'
   resources :tuits, only: [:show]
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+  get '/like/:id', to: 'tuits#like'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
